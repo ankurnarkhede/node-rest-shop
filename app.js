f368ed4bd3add321fe53196a41af2a614af13787
Ankur Narkhede <ankurnarkhede@gmail.com>
2018-06-30 07:26:02 +0530

    5

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        "Access-Control-Allow-Origin",
+        "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use('/products', productRouter);
 app.use('/orders', orderRouter);
 
