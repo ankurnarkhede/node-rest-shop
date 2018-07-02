a0adc921f55c356f87b00a583711c45b0a59712e
Ankur Narkhede <ankurnarkhede@gmail.com>
2018-07-02 08:03:38 +0530

    6

--- api/routes/products.js
+++ api/routes/products.js
@@ -10,6 +10,27 @@ const Product = require('../../models/product');
 
 // handle get requests to products
 router.get('/', function (req, res, next) {
+
+    Product.find(function (err, result) {
+        if (err) {
+            console.log(err);
+            res.status(404).json(
+                {
+                    error: err
+                });
+        }
+        if (result) {
+            console.log(result);
+            res.status(200).json({
+                message: 'Handling GET requests to /products/',
+                temp: 'temp',
+                product: result
+            });
+        }
+    });
+
+
+
     res.status(200).json({
         message: 'Handling GET requests to /products'
     });
@@ -29,6 +50,9 @@ router.post('/', function (req, res, next) {
     product.save(function (err, result) {
         if (err) {
             console.log(err);
+            res.status(500).json({
+                error: err
+            });
         }
         if (result) {
             console.log(result);
@@ -51,10 +75,10 @@ router.get('/:productId', function (req, res, next) {
     Product.findOne({_id: id}, function (err, result) {
         if (err) {
             console.log(err);
-            res.status(500).json(
+            res.status(404).json(
                 {
                     error: err,
-                    msg: 'an errror'
+                    msg: 'No valid entry found for ID: ' + id
                 });
         }
         if (result) {
