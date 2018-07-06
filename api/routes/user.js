7b4c1efc6ded088236fab0759a802b070892de4f
Ankur Narkhede <ankurnarkhede@gmail.com>
2018-07-06 10:51:18 +0530

    14] Separating controllers and JWT auth

--- api/routes/user.js
+++ api/routes/user.js
@@ -12,132 +12,19 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../../models/user');
 
+const UserController = require('../controllers/user_controller');
+
+
 
 // post to /signup /
-router.post('/signup', function (req, res, next) {
-
-    User.find({email: req.body.email}, function (er, result_user) {
-        if (result_user.length >= 1) {
-
-            return res.status(409).json({
-                message: "Email exists"
-            });
-
-        } else {
-
-            bcrypt.hash(req.body.password, 10, function (err, hash) {
-                if (err) {
-                    return res.status(500).json({
-                        error: err
-                    });
-                } else {
-                    const user = new User({
-                        _id: new mongoose.Types.ObjectId(),
-                        email: req.body.email,
-                        password: hash,
-                        password_text: req.body.password
-
-                    });
-                    user.save(function (err, result) {
-
-                        if (result) {
-                            console.log(result);
-                            res.status(200).json({
-                                message: 'Creates user in POST /signup',
-                                product: {
-                                    _id: result.id,
-                                    email: result.email,
-                                    password: result.password,
-                                    password_text: result.password_text
-                                }
-                            });
-                        } else if (err) {
-                            console.log(err);
-                            res.status(500).json({
-                                error: err
-                            });
-                        }
-
-                    });
-                }
-
-            });
-
-        }
-    });
-
-
-});
+router.post('/signup', UserController.user_post_signup);
 
 
 // post to /login /
-router.post('/login', function (req, res, next) {
-
-    User.find({email: req.body.email}, function (err, result) {
-        if (result.length < 1) {
-
-            console.log(err);
-            res.status(401).json({
-                message: 'Auth Failed'
-
-            });
-
-        }
-        bcrypt.compare(req.body.password, result[0].password, function (err_pass, result_pass) {
-            if (err_pass) {
-                return res.status(401).json({
-                    message: "Auth Failed"
-                });
-            } else if (result_pass) {
-                const token = jwt.sign({
-                        email: result[0].email,
-                        userId: result[0]._id
-                    },
-                    process.env.JWT_KEY,
-                    {
-                        expiresIn: "1h"
-                    }
-                );
+router.post('/login', UserController.user_post_login);
 
 
-                return res.status(200).json({
-                    message: "Auth Successful",
-                    token: token
-                });
-            }
-        });
-    });
-
-
-});
-
-
-
-router.delete('/:userId', function (req, res, next) {
-
-    const id = req.params.userId;
-    console.log('=========deleting user========');
-
-    // by deleting, just setting the status of user to false,
-    // not actually deleting the user
-
-    User.update({_id: id}, {status: false}, function (err, result) {
-        if (result) {
-            console.log(result);
-            res.status(200).json({
-                message: "User deleted",
-
-            });
-        }
-        else if (err) {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        }
-    });
-
-
-});
+// delete a user
+router.delete('/:userId', UserController.user_post_delete);
 
 module.exports = router;
